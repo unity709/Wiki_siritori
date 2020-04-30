b58ccd3f0339b824101a853e004ae76fa53e9994
unity709 <52840507+unity709@users.noreply.github.com>
2020-04-30 15:51:27 +0900

    hgf

--- serviceWorker.js
+++ serviceWorker.js
@@ -7,9 +7,11 @@ const urlsToCache = [
   './jquery-3.4.1.min.js',
   './icons/human_icon.png',
   './icons/Wikipedia-logo-v2-ja.png',
-  'https://kit.fontawesome.com/ede971a779.js'
+  './ede971a779.js'
 ];
-
+const CACHE_KEYS = [
+  CACHE_NAME
+];
 self.addEventListener('install', function(event) {
   event.waitUntil(
       caches.open(CACHE_NAME)
@@ -19,16 +21,83 @@ self.addEventListener('install', function(event) {
           })
   );
 });
-
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(
-      function (response) {
-          if (response) {
-              return response;
-          }
-          return fetch(event.request);
-      })
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => {
+          return !CACHE_KEYS.includes(key);
+        }).map(key => {
+          // 不要なキャッシュを削除
+          return caches.delete(key);
+        })
+      );
+    })
   );
+});
+self.addEventListener('fetch', function(event) {
+  //ブラウザが回線に接続しているかをboolで返してくれる
+  var online = navigator.onLine;
+
+  //回線が使えるときの処理
+  if(online){
+    event.respondWith(
+      caches.match(event.request)
+        .then(
+        function (response) {
+          if (response) {
+            return response;
+          }
+          //ローカルにキャッシュがあればすぐ返して終わりですが、
+          //無かった場合はここで新しく取得します
+          return fetch(event.request)
+            .then(function(response){
+              // 取得できたリソースは表示にも使うが、キャッシュにも追加しておきます
+              // ただし、Responseはストリームなのでキャッシュのために使用してしまうと、ブラウザの表示で不具合が起こる(っぽい)ので、複製しましょう
+              cloneResponse = response.clone();
+              if(response){
+                //ここ&&に修正するかもです
+                if(response || response.status == 200){
+                  //現行のキャッシュに追加
+                  caches.open(CACHE_NAME)
+                    .then(function(cache)
+                    {
+                      cache.put(event.request, cloneResponse)
+                      .then(function(){
+                        //正常にキャッシュ追加できたときの処理(必要であれば)
+                      });
+                    });
+                }else{
+                  //正常に取得できなかったときにハンドリングしてもよい
+                  return response;
+                }
+                return response;
+              }
+            }).catch(function(error) {
+              //デバッグ用
+              return console.log(error);
+            });
+        })
+    );
+  }else{
+    //オフラインのときの制御
+    event.respondWith(
+      caches.match(event.request)
+        .then(function(response) {
+          // キャッシュがあったのでそのレスポンスを返す
+          if (response) {
+            return response;
+          }
+          //オフラインでキャッシュもなかったパターン
+          return caches.match("index.html")
+              .then(function(responseNodata)
+              {
+                //適当な変数にオフラインのときに渡すリソースを入れて返却
+                //今回はoffline.htmlを返しています
+                return responseNodata;
+              });
+        }
+      )
+    );
+  }
 });
